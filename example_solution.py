b568815f0eb475d5489201bd3fb57cae1b0cd251
David Brownman <beamneocube@gmail.com>
2017-12-04 23:30:43 -0800

    added starter generator

---
+++ example_solution.py
@@ -0,0 +1,13 @@
+# problem description
+
+from base import BaseSolution
+
+class Solution(BaseSolution):
+    def part_1(self):
+        pass
+
+    def part_2(self):
+        pass
+
+    def solve(self, f):
+        pass
