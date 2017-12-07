c560de69bb9de38e1d142a1d1ccee682fc0f492a
David Brownman <beamneocube@gmail.com>
2017-12-06 23:10:05 -0800

    day 6

---
+++ code/day_6.py
@@ -0,0 +1,38 @@
+# problem description
+
+from base import BaseSolution, InputTypes
+
+class Solution(BaseSolution):
+    def input_type(self):
+        return InputTypes.TSV
+
+    def part_1(self):
+        return self.solve()[0]
+
+    def part_2(self):
+        return self.solve()[1]
+
+    def redistribute(self, arr):
+        biggest = max(arr)
+        i = arr.index(biggest)
+        arr[i] = 0
+        i += 1
+        while biggest > 0:
+            arr[i % len(arr)] += 1
+            i += 1
+            biggest -= 1
+
+        return arr
+
+    def solve(self):
+        arr = self.input[0]  # don't mutate input
+        seen = [arr[:]]
+
+        while True:  # python could use a do-while
+            arr = self.redistribute(arr)
+            if arr in seen:
+                break
+            else:
+                seen.append(arr[:])
+
+        return (len(seen), len(seen) - seen.index(arr))
