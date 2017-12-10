9f30bef22a5c37c012d72b29c64c257917b24d16
David Brownman <beamneocube@gmail.com>
2017-12-09 23:07:26 -0600

    day[9]

---
+++ code/day_9.py
@@ -0,0 +1,36 @@
+# prompt: https://adventofcode.com/2017/day/9
+
+from base import BaseSolution, InputTypes
+
+class Solution(BaseSolution):
+    def solve(self):
+        res = 0
+        garbage = False
+        garbage_count = 0
+        depth = 0
+        i = 0
+        while i < len(self.input):
+            char = self.input[i]
+
+            if char == '!':
+                i += 1
+
+            elif garbage and char != '>':
+                garbage_count += 1
+
+            elif char == '{':
+                depth += 1
+
+            elif char == '}':
+                res += depth
+                depth -= 1
+
+            elif char == '<':
+                garbage = True
+
+            elif char == '>':
+                garbage = False
+
+            i += 1
+
+        return res, garbage_count
