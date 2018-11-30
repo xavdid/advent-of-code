bbcc62594603c2ba0599639b68bf3c29b8ccafba
David Brownman <beamneocube@gmail.com>
2018-11-29 23:13:26 -0500

    day 16

---
+++ code/day_16.py
@@ -0,0 +1,14 @@
+# prompt: https://adventofcode.com/2017/day/16
+
+from .base import BaseSolution, InputTypes
+
+
+class Solution(BaseSolution):
+    def part_1(self):
+        pass
+
+    def part_2(self):
+        pass
+
+    def solve(self):
+        pass
