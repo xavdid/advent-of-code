de2fe7b34d943d6958c68e4207e4b346d5ffc862
David Brownman <beamneocube@gmail.com>
2020-09-25 15:55:33 -0700

    2019, day 14

--- solutions/2019/code/day_14.py
+++ solutions/2019/code/day_14.py
@@ -3,6 +3,7 @@
 from collections import defaultdict
 from dataclasses import dataclass
 from math import ceil
+from os import name
 from typing import Dict, List, Tuple, Union
 
 from ...base import BaseSolution, InputTypes
@@ -53,14 +54,14 @@ class Solution(BaseSolution):
     input_type = InputTypes.ARRAY
     reactions: Dict[str, Reaction] = {}
 
-    def part_1(self):
+    def ore_cost_for_n_fuel(self, num_fuel):
         self.reactions = {
             reaction.name: reaction
             for reaction in [Reaction(raw_reaction) for raw_reaction in self.input]
         }
 
         stockpile = defaultdict(int)
-        unresolved_resources = defaultdict(int, {"FUEL": 1})
+        unresolved_resources = defaultdict(int, {"FUEL": num_fuel})
         total_ore_required = 0
 
         while unresolved_resources:
@@ -75,64 +76,74 @@ class Solution(BaseSolution):
             )[0]
             reaction = self.reactions[_resource_name]
 
-            self.newline()
-            self.pp(f"working {reaction.name}")
-            if reaction.is_ore_based:
-                need_to_create = num_resources_unresolved - stockpile[reaction.name]
-                num_reactions = ceil(need_to_create / reaction.out.amount)
-                if num_reactions > 0:
-                    self.pp(
-                        f"have {stockpile[reaction.name]}, need {num_resources_unresolved}. I'm short {need_to_create}, which takes {num_reactions} reaction(s) that each produce {reaction.out.amount}"
-                    )
+            self.pp(
+                f"working {_resource_name}; have {stockpile[reaction.name]}, need {unresolved_resources[reaction.name]}"
+            )
 
-                    new_ore = reaction.ore_cost * num_reactions
-                    total_ore_required += new_ore
-                    self.pp(
-                        f"using {new_ore} more ore, total is now {total_ore_required}"
-                    )
+            need_to_create = num_resources_unresolved - stockpile[reaction.name]
 
-                    surplus_resources = (
-                        num_reactions * reaction.out.amount
-                    ) - need_to_create
-
-                    self.pp(f"had {surplus_resources} extra")
-                    stockpile[reaction.name] = surplus_resources
-                else:
-                    self.pp("have enough, skipping reactions", newline=True)
-                    stockpile[reaction.name] -= num_resources_unresolved
-
+            if need_to_create <= 0:
+                stockpile[reaction.name] -= unresolved_resources[reaction.name]
                 self.pp(
-                    f"resolving {num_resources_unresolved} {reaction.name} ({unresolved_resources[reaction.name]} -> {unresolved_resources[reaction.name] - num_resources_unresolved})"
+                    f"no reaction needed, stockpile is now {stockpile[reaction.name]}"
                 )
+                del unresolved_resources[reaction.name]
+                continue
 
-            else:
-                self.pp(f"breaking down {reaction.name}")
-                for ingredient in reaction.in_:
-                    self.pp(
-                        f"added {ingredient.amount * ceil(num_resources_unresolved / reaction.out.amount)} {ingredient.name} ({ingredient.amount} * ceil({num_resources_unresolved} / {reaction.out.amount}))"
-                    )
-                    unresolved_resources[ingredient.name] += ingredient.amount * ceil(
-                        num_resources_unresolved / reaction.out.amount
-                    )
-
+            # have too few, must create
+            self.pp(f"resolving {reaction.name}, clearing stockpile")
             del unresolved_resources[reaction.name]
-            self.pp(f"removed need for {reaction.name}")
+            del stockpile[reaction.name]
 
-        # self.pp("before", stockpile, total_ore_required)
-        # for resource, amount in stockpile.items():
-        #     reaction = self.reactions[resource]
-        #     while amount >= reaction.out.amount and reaction.is_ore_based:
-        #         # made too many, scale it back
-        #         amount -= reaction.out.amount
-        #         stockpile[resource] -= reaction.out.amount  # for logging purposes
-        #         total_ore_required -= reaction.ore_cost
+            num_reactions = ceil(need_to_create / reaction.out.amount)
+            extra = (num_reactions * reaction.out.amount) - need_to_create
+            # this reaction hasn't happened yet, but any surplus we create here could enable downstream reactions to produce less
+            # tracking this extra was what I wasn't doing before
+            # cheers to https://0xdf.gitlab.io/adventofcode2019/14#part-1 for the tip
+            stockpile[reaction.name] += extra
+            self.pp(
+                f"doing {num_reactions} reaction(s) (each producing {reaction.out.amount}) to fulfill above, leaving {extra} extra"
+            )
 
-        # self.pp("after", stockpile, total_ore_required)
+            if reaction.is_ore_based:
+                total_ore_required += num_reactions * reaction.ore_cost
+                self.pp(
+                    f"that cost {num_reactions * reaction.ore_cost} ore, total is now {total_ore_required}"
+                )
+            else:
+                self.pp("not an ore reaction!")
+                for reagent in reaction.in_:
+                    self.pp(
+                        f"adding {reaction.name} depedency on {reagent.name} (need {reagent.amount * num_reactions})"
+                    )
+                    unresolved_resources[reagent.name] += reagent.amount * num_reactions
 
         return total_ore_required
 
-    def part_2(self):
-        pass
+    def part_1(self):
+        return self.ore_cost_for_n_fuel(1)
 
-    def solve(self):
-        pass
+    def part_2(self):
+        # need to find the input that gives output that's closest to this without going over
+        # binary search!
+        target = 1_000_000_000_000
+        result = 0
+
+        low = 1
+        high = target
+        mid = 0
+
+        while low <= high:
+            mid = (low + high) // 2
+            result = self.ore_cost_for_n_fuel(mid)
+
+            # unlikely, but possible
+            if result == target:
+                return mid
+
+            if result > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return mid
