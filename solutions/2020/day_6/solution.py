86b805b73ca1f1d3a06e0fae5c7390fdc24b0613
David Brownman <beamneocube@gmail.com>
2020-12-06 19:55:22 -0800

    2020 day 6

---
+++ solutions/2020/day_6/solution.py
@@ -0,0 +1,25 @@
+# prompt: https://adventofcode.com/2020/day/6
+
+from ...base import BaseSolution
+
+
+class Solution(BaseSolution):
+    year = 2020
+    number = 6
+
+    def part_1(self) -> int:
+        groups = self.input.split("\n\n")
+        total = 0
+        for group in groups:
+            all_chars_as_str = "".join(group.split("\n"))
+            total += len(set(all_chars_as_str))
+        return total
+
+    def part_2(self) -> int:
+        groups = self.input.split("\n\n")
+        total = 0
+        for group in groups:
+            answers = [set(x) for x in group.split("\n")]
+            total += len(answers[0].intersection(*answers[1:]))
+
+        return total
