e638de42692c7780fdf19590ae6503c93d9c2129
David Brownman <beamneocube@gmail.com>
2020-12-19 00:31:17 -0800

    2020 day 16

--- solutions/2020/day_16/solution.py
+++ solutions/2020/day_16/solution.py
@@ -1,6 +1,7 @@
 # prompt: https://adventofcode.com/2020/day/16
 
-from typing import Set, Tuple
+from math import prod
+from typing import List, Sequence, Set, Tuple
 
 from ...base import BaseSolution
 
@@ -9,7 +10,6 @@ class Solution(BaseSolution):
     year = 2020
     number = 16
     validators = {}
-    # input_type = InputTypes.
 
     def parse_requirements(self) -> None:
         requirements_block = self.input.split("\n\n")[0]
@@ -34,69 +34,65 @@ class Solution(BaseSolution):
                     return True
         return False
 
-    def is_valid_ticket(self, ticket: str) -> bool:
-        return all([self.is_valid_value(int(v)) for v in ticket.split(",")])
-
-    def possible_fields_for_value(self, value: int) -> Set[str]:
-        return {
-            field
-            for field, validators in self.validators.items()
-            if any([value in r for r in validators])
-        }
-
-    def part_1(self) -> int:
-        self.parse_requirements()
-        nearby_tickets_block = self.input.split("\n\n")[2]
-        total = 0
-        for line in nearby_tickets_block.split("\n")[1:]:
-            for v in line.split(","):
-                if not self.is_valid_value(int(v)):
-                    total += int(v)
-
-        return total
-
-    def part_2(self) -> int:
-        self.parse_requirements()
-        tickets = self.input.split("\n\n")[2].split("\n")[1:]
-        valid_tickets = [
-            list(map(int, t.split(","))) for t in tickets if self.is_valid_ticket(t)
+    def possible_columns_for_values(self, value_set) -> List[str]:
+        return [
+            field_name
+            for field_name, validators in self.validators.items()
+            if self.all_values_pass_validators(value_set, validators)
         ]
 
-        answer = {}
+    # pylint: disable=no-self-use
+    def all_values_pass_validators(
+        self, value_set, validators: List[Sequence[int]]
+    ) -> bool:
+        return all([any(v in validator for validator in validators) for v in value_set])
 
-        for ticket in valid_tickets:
-            for index, value in enumerate(ticket):
-                possible_fields = self.possible_fields_for_value(value)
-                if len(possible_fields) == 1:
-                    answer[index] = possible_fields.pop()
-                    print(f"value {index} is {answer[index]}")
-
-        # any uniquely identified fields have now been found
-
+    # pylint: disable=too-many-locals
     def solve(self) -> Tuple[int, int]:
         self.parse_requirements()
 
         nearby_tickets_block = self.input.split("\n\n")[2]
-        total = 0
-        valid_tickets = []
+        total_invalid_value = 0
+        valid_tickets: List[List[int]] = []
         for line in nearby_tickets_block.split("\n")[1:]:
             ticket = list(map(int, line.split(",")))
             valid = True
             for v in ticket:
                 if not self.is_valid_value(v):
                     valid = False
-                    total += int(v)
+                    total_invalid_value += v
 
             if valid:
                 valid_tickets.append(ticket)
 
         # --- part 2
-        answer = {}
-        for ticket in valid_tickets:
-            for index, value in enumerate(ticket):
-                possible_fields = self.possible_fields_for_value(value)
-                if len(possible_fields) == 1:
-                    answer[index] = possible_fields.pop()
-                    print(f"value {index} is {answer[index]}")
 
-        return total, len(valid_tickets)
+        value_sets: List[Set[int]] = [
+            {ticket[column] for ticket in valid_tickets}
+            for column in range(len(self.validators))
+        ]
+
+        column_names = {}
+        while self.validators:
+            self.pp(f"Top of loop, {len(self.validators)} unknown fields remain")
+            for index, value_set in enumerate(value_sets):
+                possible_columns = self.possible_columns_for_values(value_set)
+                if len(possible_columns) == 1:
+                    field_name = possible_columns[0]
+                    column_names[field_name] = index
+                    self.pp(f'field "{field_name}" identified at column {index}')
+                    self.validators.pop(field_name)
+
+        my_ticket = list(
+            map(int, self.input.split("\n\n")[1].split("\n")[1].split(","))
+        )
+
+        departure_info = prod(
+            [
+                my_ticket[field_index]
+                for field_name, field_index in column_names.items()
+                if field_name.startswith("departure")
+            ]
+        )
+
+        return total_invalid_value, departure_info
