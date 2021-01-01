706128eaaaaf9b72e08011bba1ddeb470b0af876
David Brownman <beamneocube@gmail.com>
2020-12-31 21:57:18 -0800

    2020 day 22 cleaned up

--- solutions/2020/day_22/solution.py
+++ solutions/2020/day_22/solution.py
@@ -54,7 +54,7 @@ class Solution(BaseSolution):
     next_game_num = 1
 
     def play_game(
-        self, a: MaybeDeck, b: MaybeDeck, is_root_game=False, game=1
+        self, a: MaybeDeck, b: MaybeDeck, is_root_game=False
     ) -> Union[int, bool]:
         """
         this is a bad return type, but it's fine
@@ -65,23 +65,15 @@ class Solution(BaseSolution):
         player_2 = Deck(b)
 
         winner: Optional[Deck] = None
-        round_num = 1
-        self.pp("\nSubgame")
-        self.next_game_num += 1
-        player_1_decks = set()  # each deck configuraiton
-        player_2_decks = set()  # each deck configuraiton
+        player_1_decks = set()
+        player_2_decks = set()
 
         while winner is None:
-            self.pp(f"Game {game}, Round {round_num}")
-            self.pp("Player 1's deck:", player_1.cards)
-            self.pp("Player 2's deck:", player_2.cards)
-
             if (
                 player_1.frozen_deck in player_1_decks
                 and player_2.frozen_deck in player_2_decks
             ):
                 # if we've played it before, player 1 wins
-                self.pp(f"Player 1 wins by replay (game {game} round {round_num})")
                 winner = player_1
                 break
 
@@ -91,9 +83,7 @@ class Solution(BaseSolution):
             if player_1.can_play_subgame and player_2.can_play_subgame:
                 # recurse
                 player_1_won = self.play_game(
-                    player_1.subgame_deck,
-                    player_2.subgame_deck,
-                    game=self.next_game_num,
+                    player_1.subgame_deck, player_2.subgame_deck
                 )
                 if player_1_won:
                     round_winner = player_1
@@ -110,16 +100,9 @@ class Solution(BaseSolution):
                 round_loser = player_1
 
             round_winner.win_round(round_loser.cards.popleft())
-            self.pp(
-                f'Player {"1" if round_winner == player_1 else "2"} wins game {game} round {round_num}!\n'
-            )
 
             if round_loser.lost:
                 winner = round_winner
-
-            round_num += 1
-
-        self.pp("game over, back up the chain\n\n")
 
         if is_root_game:
             return winner.score()
@@ -147,6 +130,7 @@ class Solution(BaseSolution):
                 winner = round_winner
 
         answer = winner.score()
+
         assert answer == 32083
         return answer
 
@@ -156,5 +140,4 @@ class Solution(BaseSolution):
         answer = self.play_game(blocks[0], blocks[1], is_root_game=True)
 
         assert answer == 35495
-
         return answer
