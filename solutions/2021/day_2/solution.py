845ffc13d7042d80a2d90812e2ae9fae0fabfb0a
David Brownman <beamneocube@gmail.com>
2021-12-02 14:57:06 -0800

    2021 day 2

---
+++ solutions/2021/day_2/solution.py
@@ -0,0 +1,53 @@
+# prompt: https://adventofcode.com/2021/day/2
+
+from ...base import StrSplitSolution, answer
+
+# from typing import Tuple
+
+
+class Solution(StrSplitSolution):
+    _year = 2021
+    _day = 2
+
+    @answer(1714950)
+    def part_1(self) -> int:
+        depth = 0
+        horiz = 0
+
+        for ins in self.input:
+            direction, value_str = ins.split(" ")
+            value = int(value_str)
+            if direction == "forward":
+                horiz += value
+            elif direction == "up":
+                depth -= value
+            elif direction == "down":
+                depth += value
+            else:
+                raise ValueError(f'Unrecognized direction: "{direction}"')
+
+        return depth * horiz
+
+    @answer(1281977850)
+    def part_2(self) -> int:
+        depth = 0
+        horiz = 0
+        aim = 0
+
+        for ins in self.input:
+            direction, value_str = ins.split(" ")
+            value = int(value_str)
+            if direction == "forward":
+                horiz += value
+                depth += aim * value
+            elif direction == "up":
+                aim -= value
+            elif direction == "down":
+                aim += value
+            else:
+                raise ValueError(f'Unrecognized direction: "{direction}"')
+
+        return depth * horiz
+
+    # def solve(self) -> Tuple[int, int]:
+    #     pass
