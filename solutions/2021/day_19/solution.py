ef9ee4f6179c08baff16a3eed3c28797ad3dff93
David Brownman <beamneocube@gmail.com>
2021-12-27 13:17:18 -0800

    2021 skip day 19

---
+++ solutions/2021/day_19/solution.py
@@ -0,0 +1,20 @@
+# prompt: https://adventofcode.com/2021/day/19
+
+from ...base import TextSolution, answer
+# from typing import Tuple
+
+class Solution(TextSolution):
+    _year = 2021
+    _day = 19
+
+    # @answer(1234)
+    def part_1(self) -> int:
+        pass
+
+    # @answer(1234)
+    def part_2(self) -> int:
+        pass
+
+    # @answer((1234, 4567))
+    # def solve(self) -> Tuple[int, int]:
+    #     pass
