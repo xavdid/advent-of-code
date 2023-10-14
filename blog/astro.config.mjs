68aa77d0d76163d7a5a844996d2dd808d4814cc4
David Brownman <beamneocube@gmail.com>
2023-10-13 21:37:51 -0700

    autolink headers

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -1,8 +1,14 @@
+import { rehypeHeadingIds } from "@astrojs/markdown-remark";
 import { defineConfig } from "astro/config";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
+    rehypePlugins: [
+      rehypeHeadingIds,
+      [rehypeAutolinkHeadings, { behavior: "wrap" }],
+    ],
     shikiConfig: {
       theme: "monokai",
     },
